7d4d0e5b95af955ef5af570fe417a02d76a7f91e
sreepathi-ravikumar <rksreepathi1@gmail.com>
2025-07-06 12:33:17 +0530

    Update server.js

--- server.js
+++ server.js
@@ -15,5 +15,5 @@ app.post("/ask", async (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(✅ Server running at http://localhost:${port});
+  console.log('✅ Server running at http://localhost:${port}');
 });
