6698c9715ff84fa9d5e926e4c5b01d67d1584daa
sreepathi-ravikumar <rksreepathi1@gmail.com>
2025-07-06 11:35:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,5 +18,4 @@ def ask():
     return jsonify({"answer": result})
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 10000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=5000)
